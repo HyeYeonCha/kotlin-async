e42b8a1046589d3c6a6494cfaaac8c1367da3b2a
HyeYeonCha <gpdus335@naver.com>
2022-10-18 22:43:00 +0900

    Iterator 패턴 코멘트 추가

--- src/main/kotlin/iteratorpattern/Car.kt
+++ src/main/kotlin/iteratorpattern/Car.kt
@@ -3,6 +3,8 @@ package iteratorpattern
 
 data class Car(val brand: String)
 
+// Iterator -> 내부에 데이터를 저장하고 데이터를 순차적으로 pull 해서 가져옴
+// Observer -> 데이터 제공자가 소비하는 측의 데이터를 통제하는 push 기반
 class CarIterable(val cars: List<Car> = listOf()) : Iterable<Car> {
 
     override fun iterator(): Iterator<Car> = CarIterator(cars)
