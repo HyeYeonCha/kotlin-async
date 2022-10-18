c48bf0c2e166aa35ac099560a6d59117b1d332fc
HyeYeonCha <gpdus335@naver.com>
2022-10-18 22:36:44 +0900

    Observer 패턴

--- src/main/kotlin/observerpattern/ObserverPattern.kt
+++ src/main/kotlin/observerpattern/ObserverPattern.kt
@@ -4,7 +4,8 @@ import java.util.*
 
 class Coffee(val name: String)
 
-// Subject
+// deprecated 뭐냐
+// Subject (Observer 로 변경 상태 발행)
 class Barista : Observable() {
 
     private lateinit var coffeeName: String
@@ -14,18 +15,20 @@ class Barista : Observable() {
     }
 
     fun makeCoffee() {
-        setChanged()
-        notifyObservers(Coffee(this.coffeeName))
+        setChanged() // Observable 의 플래그
+        notifyObservers(Coffee(this.coffeeName)) // Observer 로 객체 전달
     }
 
 }
 
-// Observer
+// Observer (소비자)
 class Customer(val name: String) : Observer {
 
+    // Observer 의 동작
     override fun update(o: Observable?, arg: Any?) {
         val coffee = arg as Coffee
         println("${name}이 ${coffee.name}을 받았습니다")
+        println(o)
     }
 }
 
@@ -38,9 +41,9 @@ fun main() {
     val customer2 = Customer("고객2")
     val customer3 = Customer("고객3")
 
-    barista.addObserver(customer)
+    barista.addObserver(customer) // Observer 등록
     barista.addObserver(customer2)
-    barista.addObserver(customer)
+    barista.addObserver(customer3)
 
-    barista.makeCoffee()
+    barista.makeCoffee() // Observer 실행해주는 함수
 }
